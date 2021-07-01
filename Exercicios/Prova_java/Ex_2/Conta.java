1d8e0e95fa1a90f1d0b26f71fc144e01480f0c07
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2021-07-01 13:00:35 -0300

    Classes sacar

--- Exercicios/Prova_java/Ex_2/Conta.java
+++ Exercicios/Prova_java/Ex_2/Conta.java
@@ -1,3 +1,58 @@
-public class conta_banco_classe {
+
+
+class Conta{
+
+    String nome;
+    int    numero;
+    String agencia;
+    double saldo;
+    String criacao;
+            
+    //class construtora
+    public Conta(String nome, int numero, String agencia, double saldo, String criacao){
+
+        this.nome     = nome;
+        this.numero   = numero;
+        this.agencia  = agencia;
+        this.saldo    = saldo;
+        this.criacao  = criacao;
+    }
+
+    //CLASSES PADRÃO
+    public AlterarNome(){
+        this.nome = nome;
+    } 
+
+    //Classes de retorno 
+    public String RetornoNome(){
+        return this.nome = nome;       
+    }
+
+    public int RetornoNumero(){
+        return this.numero = numero; 
+    }
+
+    public String RetornoAgencia(){
+        return this.agencia = agencia;       
+    }
     
+    public double RetornoSaldo(){
+        return this.saldo = saldo;
+    }
+    
+    public String RetornoCriacao(){
+        return this.criacao = criacao;
+    }
+
+
+    //Saque
+    public void Sacar(double saque){
+
+        this.saldo -=saque;
+        
+        System.out.println("Você sacou: "+ saque +"R$ \nSeu saldo atual é de: " + saldo +"R$");
+
+    }
+
+
 }
